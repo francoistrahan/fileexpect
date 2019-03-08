2a6b5cc1b95b0cf37684b758e7f37c47b814a51c
François Trahan <francois.trahan@gmail.com>
2019-03-08 12:19:43 -0500

    Base class for comparisson

---
+++ pytest_fileexpect/FileBasedComparer.py
@@ -0,0 +1,31 @@
+from abc import ABC
+from pathlib import Path
+
+
+
+FILENAME_FORMAT = "{}.{}"
+
+
+
+class FileBasedComparer(ABC):
+    def __init__(self, contentRoot: Path, fileExtension: str):
+        self.fileExtension = fileExtension
+        self.contentRoot = contentRoot
+
+
+    def matches(self, contentName, actual):
+        if self.fileExtension:
+            contentName = FILENAME_FORMAT.format(contentName, self.fileExtension)
+        path = self.contentRoot / contentName
+
+        assert path.exists() and path.is_file()
+
+        expected = self.readContent(path)
+
+        assert self.areEqual(expected, actual)
+
+
+    def readContent(self, path): pass
+
+
+    def areEqual(self, expected, actual): pass
